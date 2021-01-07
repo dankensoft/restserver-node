4ec346e942d7162abafd282a18533800679461f1
Danniels Castillo <danniels.magana@gmail.com>
2021-01-07 13:40:36 -0600

    Productos y Categorias

--- server/config/config.js
+++ server/config/config.js
@@ -1,16 +1,33 @@
-// ===========================
-//           Puerto
-// ===========================
+// ============================
+//  Puerto
+// ============================
 process.env.PORT = process.env.PORT || 3000;
 
-// ===========================
-//           Entorno
-// ===========================
+
+// ============================
+//  Entorno
+// ============================
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
-// ===========================
-//           Base de Datos
-// ===========================
+
+// ============================
+//  Vencimiento del Token
+// ============================
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 días
+process.env.CADUCIDAD_TOKEN = '48h';
+
+
+// ============================
+//  SEED de autenticación
+// ============================
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
+// ============================
+//  Base de datos
+// ============================
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
@@ -18,24 +35,9 @@ if (process.env.NODE_ENV === 'dev') {
 } else {
     urlDB = process.env.MONGO_URI;
 }
-
 process.env.URLDB = urlDB;
 
-// ===========================
-//    Vencimiento del Token
-// ===========================
-// 60 Segundos
-// 60 Minutos
-// 24 Horas
-// 30 Días
-process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
-
 // ============================
-// SEED/SECRET de Autenticación
+//  Google Client ID
 // ============================
-process.env.SEED = process.env.SEED || 'secret';
-
-// ============================
-// Google Client ID
-// ============================
-process.env.CLIENT_ID = process.env.CLIENT_ID || '948481818791-a4a5pquo60eiq9j0hd5kk61ie3suh5u6.apps.googleusercontent.com';
+process.env.CLIENT_ID = process.env.CLIENT_ID || '219758474264-vh1bibcphgvbc32km508lubtqkanikf1.apps.googleusercontent.com';
